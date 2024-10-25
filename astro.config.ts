81704dc2cf634794eee4f4eec5627690837053db
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-10-25 10:24:04 +0000

    uno preset

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
+import unoCss from 'unocss/astro'
 
 export default defineConfig({
+  integrations: [unoCss()],
   trailingSlash: 'always'
 })
--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,62 @@
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
-  presets: []
+  presets: [
+    {
+      name: 'position',
+      rules: [
+        [
+          'pos-fixed',
+          {
+            position: 'fixed'
+          }
+        ],
+        [
+          /^(?<direction>left|top)-(?<value>0)$/,
+          (match) => {
+            if (match.groups?.['direction'] && match.groups['value']) {
+              return {
+                [match.groups['direction']]: match.groups['value']
+              }
+            }
+            return {}
+          },
+          {
+            autocomplete: [
+              'left-0',
+              'top-0'
+            ]
+          }
+        ]
+      ]
+    },
+    {
+      name: 'width',
+      rules: [
+        [
+          'w-4',
+          {
+            width: '1rem'
+          }
+        ],
+        [
+          'w-full',
+          {
+            width: '100%'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'z-index',
+      rules: [
+        [
+          'z-1',
+          {
+            'z-index': 1
+          }
+        ]
+      ]
+    }
+  ]
 })
