4c3f04c5b585b7ff21cdb02e95e440214a11f480
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-10-26 09:21:54 +0000

    add more rules

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config'
 import unoCss from 'unocss/astro'
 
 export default defineConfig({
+  build: {
+    inlineStylesheets: 'never'
+  },
   integrations: [unoCss()],
   trailingSlash: 'always'
 })
--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,94 @@
 import { defineConfig } from 'unocss'
 
+function valueToRem(n = '0') {
+  return `${Number.parseFloat(n) * 4}rem`
+}
+
 export default defineConfig({
   presets: [
+    {
+      name: 'background-color',
+      rules: [
+        [
+          /^bg-(?<color>.+)-(?<intensity>500)$/,
+          (match, context) => {
+            if (
+              match.groups &&
+              Object.keys(context.theme.colors).includes(
+                match.groups['color'] as string
+              )
+            ) {
+              return {
+                'background-color':
+                  context.theme.colors[
+                    match.groups['color'] as keyof typeof context.theme.colors
+                  ][match.groups['intensity'] as '500']
+              }
+            }
+            return {}
+          },
+          {
+            autocomplete: ['bg-dark-500']
+          }
+        ]
+      ]
+    },
+    {
+      name: 'border',
+      rules: [
+        [
+          'border-solid',
+          {
+            'border-style': 'solid'
+          }
+        ],
+        [
+          /^border-(?:(?<direction>bottom|top)-)?(?<value>[01])$/,
+          (match) => {
+            if (match.groups) {
+              const value = `${match.groups['value']}px`
+              if (match.groups['direction']) {
+                return {
+                  [`border-${[match.groups['direction'] as 'bottom' | 'top']}-width`]:
+                    value
+                }
+              }
+              return {
+                'border-width': value
+              }
+            }
+            return {}
+          },
+          {
+            autocomplete: [
+              'border-0',
+              'border-1',
+              'border-bottom-0',
+              'border-bottom-1',
+              'border-top-0',
+              'border-top-1'
+            ]
+          }
+        ]
+      ]
+    },
+    {
+      name: 'dark-mode',
+      variants: [
+        {
+          match(matcher) {
+            if (!matcher.startsWith('dark:')) {
+              return matcher
+            }
+            return {
+              matcher: matcher.slice(5),
+              parent: '.dark'
+            }
+          },
+          name: 'dark'
+        }
+      ]
+    },
     {
       name: 'position',
       rules: [
@@ -14,18 +101,17 @@ export default defineConfig({
         [
           /^(?<direction>left|top)-(?<value>0)$/,
           (match) => {
-            if (match.groups?.['direction'] && match.groups['value']) {
+            if (match.groups) {
               return {
-                [match.groups['direction']]: match.groups['value']
+                [match.groups['direction'] as 'left' | 'top']: valueToRem(
+                  match.groups['value']
+                )
               }
             }
             return {}
           },
           {
-            autocomplete: [
-              'left-0',
-              'top-0'
-            ]
+            autocomplete: ['left-0', 'top-0']
           }
         ]
       ]
@@ -58,5 +144,12 @@ export default defineConfig({
         ]
       ]
     }
-  ]
+  ],
+  theme: {
+    colors: {
+      dark: {
+        500: '#1f1f1f'
+      }
+    }
+  }
 })
