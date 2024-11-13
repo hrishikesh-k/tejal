85d5b711d2a8ac6892f4be8af7a2d084d8364f94
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-11-13 09:25:17 +0000

    add page indicators

--- src/components/svelte/navigation.svelte
+++ src/components/svelte/navigation.svelte
@@ -4,7 +4,17 @@ import { on } from 'svelte/events'
 import Icon from '~/components/svelte/icon.svelte'
 import Tooltip from '~/components/svelte/tooltip.svelte'
 
-const mainAnchorClass = [
+let {
+  menu,
+  section
+}: {
+  menu?: 'about' | 'contact' | 'work'
+  section?: 'advertising' | 'fashion' | 'presentations'
+} = $props()
+
+const mainAnchorActiveClass = ['after:w-20', 'md:after:w-full'].join(' ')
+
+const mainAnchorBaseClass = [
   'after:bg-current',
   'after:border-rounded-0.25',
   'after:bottom-0.5',
@@ -19,15 +29,18 @@ const mainAnchorClass = [
   'text-center',
   'text-current',
   'after:transform',
-  'after:transition-duration-250',
-  'after:transition-width',
   'after:translate-x--1/2',
   'md:after:translate-x-0',
   'w-full',
+  'md:w-unset'
+].join(' ')
+
+const mainAnchorPassiveClass = [
+  'after:transition-duration-250',
+  'after:transition-width',
   'after:w-0',
   'after:hover:w-20',
-  'md:after:hover:w-full',
-  'md:w-unset'
+  'md:after:hover:w-full'
 ].join(' ')
 
 const subAnchorClass = [
@@ -36,6 +49,9 @@ const subAnchorClass = [
   'block',
   'box-border',
   'decoration-none',
+  'flex',
+  'gap-x-1',
+  'items-center',
   'p-2',
   'text-current',
   'text-right'
@@ -87,17 +103,23 @@ onMount(checkTheme)
 </script>
 
 <nav class="bg-light-500 dark:bg-dark-500 flex flex-col md:flex-row gap-x-3 items-center left-0 pos-absolute md:pos-static transition-duration-250 transition-top w-full md:w-unset" class:top-4={open} class:top--30={!open}>
-  <div class="box-border cursor-pointer flex gap-x-1 group items-center justify-center p-2 pos-relative w-full md:w-unset">
+  <div class="cursor-pointer flex gap-x-1 group items-center justify-center {mainAnchorBaseClass} {menu?.startsWith('work') && mainAnchorActiveClass}">
     <span>Work</span>
     <Icon name="caret-down" size={3}/>
     <div class="bg-light-500 dark:bg-dark-500 border-0.25 border-gray-300 dark:border-gray-500 border-rounded-1.5 border-solid bottom--26 box-border hidden group-hover:block pos-absolute right-3/8 md:right-0 z-1">
-      <a class="{subAnchorClass} border-t-rounded-1.5" href="/work/advertising/">Advertising</a>
-      <a class={subAnchorClass} href="/work/fashion/">Fashion</a>
-      <a class="{subAnchorClass} border-b-rounded-1.5" href="/work/presentations/">Presentations</a>
+      {#each ['Advertising', 'Fashion', 'Presentations'] as sec, index (sec)}
+        {@const secLower = sec.toLowerCase()}
+        <a class="{subAnchorClass} {index === 0 && 'border-t-rounded-1.5'} {index === 2 && 'border-b-rounded-1.5'}" href="/work/{secLower}/">
+          {#if secLower === section}
+            <Icon name="caret-right" size={3}/>
+          {/if}
+          <span class="block flex-basis-0 flex-grow-1 flex-shrink-1">{sec}</span>
+        </a>
+      {/each}
     </div>
   </div>
-  <a class={mainAnchorClass} href="/about/">About</a>
-  <a class={mainAnchorClass} href="/contact/">Contact</a>
+  <a class="{mainAnchorBaseClass} {menu === 'about' ? mainAnchorActiveClass : mainAnchorPassiveClass}" href="/about/">About</a>
+  <a class="{mainAnchorBaseClass} {menu === 'contact' ? mainAnchorActiveClass : mainAnchorPassiveClass}" href="/contact/">Contact</a>
 </nav>
 <Tooltip {icon} onclick={onclickTheme} text="Toggle theme"/>
 <div class="block md:hidden z-1">
