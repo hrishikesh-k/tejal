87855516b98b1941649e9f1986892bbbf708f94d
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-11-30 15:47:11 +0800

    add custom sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,9 @@
+import { statSync, writeFileSync } from 'node:fs'
+import { join } from 'node:path'
+import { cwd } from 'node:process'
 import { fileURLToPath } from 'node:url'
-import sitemap from '@astrojs/sitemap'
 import svelte from '@astrojs/svelte'
+import type { AstroConfig } from 'astro'
 import { defineConfig } from 'astro/config'
 import uno from 'unocss/astro'
 
@@ -8,7 +11,45 @@ export default defineConfig({
   build: {
     inlineStylesheets: 'never'
   },
-  integrations: [sitemap(), svelte(), uno()],
+  integrations: [
+    svelte(),
+    uno(),
+    (() => {
+      let config: AstroConfig
+      return {
+        hooks: {
+          'astro:build:done': (options) => {
+            let sitemap =
+              '<?xml encoding="UTF-8" version="1.0"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+            for (const page of options.pages.sort((a, b) =>
+              a.pathname.localeCompare(b.pathname, 'en', {
+                numeric: true
+              })
+            )) {
+              sitemap += `<url><loc>${new URL(page.pathname, config.site).href}</loc><lastmod>${statSync(
+                join(
+                  cwd(),
+                  'src',
+                  'pages',
+                  `${page.pathname === '' ? 'index' : page.pathname.slice(0, -1)}.astro`
+                )
+              ).mtime.toISOString()}</lastmod></url>`
+            }
+            sitemap += '</urlset>'
+            writeFileSync(
+              join(fileURLToPath(options.dir), 'sitemap.xml'),
+              sitemap,
+              'utf-8'
+            )
+          },
+          'astro:config:done': (options) => {
+            config = options.config
+          }
+        },
+        name: ''
+      }
+    })()
+  ],
   site: 'https://www.tejalshinde.com/',
   trailingSlash: 'always',
   vite: {
