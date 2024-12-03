dfbd427d3e4b93e3a76ba1b69357511ae7c880f3
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-12-04 01:39:27 +0800

    fix lint errors

--- src/components/svelte/navigation.svelte
+++ src/components/svelte/navigation.svelte
@@ -18,7 +18,7 @@ const mainAnchorBaseClass = [
   'after:bg-current',
   'after:border-rounded-0.25',
   'after:bottom-0.5',
-        'after:content',
+  'after:content',
   'box-border',
   'decoration-none',
   'after:h-0.5',
