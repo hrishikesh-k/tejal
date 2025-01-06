caa6d6f7eadf8c7db73fa65ce424f62e9d06ed26
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-01-06 16:47:16 +0800

    fix lint

--- netlify/edge-functions/password.ts
+++ netlify/edge-functions/password.ts
@@ -9,12 +9,12 @@ type JwtPayload = {
 }
 
 export const config: Config = {
-  method: [/*'GET', */'POST'],
+  method: [/*'GET', */ 'POST'],
   onError: 'bypass',
   pattern: [
     // '^\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?$',
     // '^\\/[Aa][Bb][Oo][Uu][Tt](?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$',
-    '^\\/[Cc][Oo][Nn][Tt][Aa][Cc][Tt](?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$',
+    '^\\/[Cc][Oo][Nn][Tt][Aa][Cc][Tt](?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$'
     // '^\\/[Ww][Oo][Rr][Kk](?:.*)?(?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$'
   ]
 }
