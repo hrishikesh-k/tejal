641919829dd5d2ad51802dc3db0f7f3d3c7965a3
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-04-10 19:57:14 +0800

    remove password

--- netlify/edge-functions/validations.ts
+++ netlify/edge-functions/validations.ts
@@ -1,28 +1,28 @@
 import type { Config, Context } from '@netlify/edge-functions'
 import { parseContactForm } from './utils/contact.ts'
-import { passwordPrompt } from './utils/gatekeeper.ts'
-import { validatePassword } from './utils/password.ts'
+// import { passwordPrompt } from './utils/gatekeeper.ts'
+// import { validatePassword } from './utils/password.ts'
 import { http400 } from './utils/responses.ts'
 
 export const config: Config = {
-  method: ['GET', 'POST'],
+  method: [/*'GET', */'POST'],
   onError: 'bypass',
   pattern: [
-    '^\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?$',
-    '^\\/[Aa][Bb][Oo][Uu][Tt](?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$',
+    /* '^\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?$',
+    '^\\/[Aa][Bb][Oo][Uu][Tt](?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$', */
     '^\\/[Cc][Oo][Nn][Tt][Aa][Cc][Tt](?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$',
-    '^\\/[Ww][Oo][Rr][Kk](?:.*)?(?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$'
+    // '^\\/[Ww][Oo][Rr][Kk](?:.*)?(?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$'
   ]
 }
 
 export default async function (req: Request, context: Context) {
   console.info(`[${req.method}] ${context.url.pathname}`)
 
-  if (req.method.toUpperCase() === 'GET') {
+  /* if (req.method.toUpperCase() === 'GET') {
     return await passwordPrompt()
-  }
+  } */
 
-  if (req.method.toUpperCase() === 'POST') {
+  // if (req.method.toUpperCase() === 'POST') {
     let body: FormData
 
     try {
@@ -33,16 +33,16 @@ export default async function (req: Request, context: Context) {
 
     const formName = body.get('form-name')
 
-    if (formName === 'password') {
+   /* if (formName === 'password') {
       return await validatePassword(body)
-    }
+    } */
 
     if (formName === 'contact') {
       return await parseContactForm(req, body)
     }
 
     return http400
-  }
+  //}
 
-  return
+  // return
 }
