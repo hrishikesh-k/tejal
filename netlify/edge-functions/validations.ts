9425acf5a6a4d0e114c1f7aeb49a0351987a5d71
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-04-10 19:57:42 +0800

    fix lint errors

--- netlify/edge-functions/validations.ts
+++ netlify/edge-functions/validations.ts
@@ -5,12 +5,12 @@ import { parseContactForm } from './utils/contact.ts'
 import { http400 } from './utils/responses.ts'
 
 export const config: Config = {
-  method: [/*'GET', */'POST'],
+  method: [/*'GET', */ 'POST'],
   onError: 'bypass',
   pattern: [
     /* '^\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?$',
     '^\\/[Aa][Bb][Oo][Uu][Tt](?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$', */
-    '^\\/[Cc][Oo][Nn][Tt][Aa][Cc][Tt](?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$',
+    '^\\/[Cc][Oo][Nn][Tt][Aa][Cc][Tt](?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$'
     // '^\\/[Ww][Oo][Rr][Kk](?:.*)?(?:\\/(?:[Ii][Nn][Dd][Ee][Xx]\\.[Hh][Tt][Mm][Ll])?|\\.[Hh][Tt][Mm][Ll])?$'
   ]
 }
@@ -23,25 +23,25 @@ export default async function (req: Request, context: Context) {
   } */
 
   // if (req.method.toUpperCase() === 'POST') {
-    let body: FormData
+  let body: FormData
 
-    try {
-      body = await req.clone().formData()
-    } catch {
-      return http400
-    }
+  try {
+    body = await req.clone().formData()
+  } catch {
+    return http400
+  }
 
-    const formName = body.get('form-name')
+  const formName = body.get('form-name')
 
-   /* if (formName === 'password') {
+  /* if (formName === 'password') {
       return await validatePassword(body)
     } */
 
-    if (formName === 'contact') {
-      return await parseContactForm(req, body)
-    }
+  if (formName === 'contact') {
+    return await parseContactForm(req, body)
+  }
 
-    return http400
+  return http400
   //}
 
   // return
