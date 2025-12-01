56f15149649932b8f1fc8fece5f12b1f0d05d1d9
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-12-01 19:12:23 +0800

    update order

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -146,6 +146,8 @@ function roundCornersLengthGt3Lg(
       [length - 3]: ['borderBottomLeftRadius'],
       [length - 1]: ['borderBottomRightRadius']
     }
+    
+    console.log(radiusMapping)
 
     setBorderRadius(
       link,
